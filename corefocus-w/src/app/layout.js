84b87f01e35698edd62e4b55ffa8afc5eae88ab7
srahman14 <s.rahman14@hotmail.com>
2025-06-11 23:07:35 +0100

    added fontawesome cdn

--- corefocus-w/src/app/layout.js
+++ corefocus-w/src/app/layout.js
@@ -17,6 +17,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
   <html lang="en" className={`${montserrat.variable}`}>
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
+          integrity="sha512-...." // Optional: add integrity for security
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
+        />
+      </head>
       <body>
         <Navbar /> 
         {children}
