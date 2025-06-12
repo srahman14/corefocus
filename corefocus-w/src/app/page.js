7a108018846bc080548f84eeccd16450b649d2cb
srahman14 <s.rahman14@hotmail.com>
2025-06-12 18:51:30 +0100

    added a component to home page

--- corefocus-w/src/app/page.js
+++ corefocus-w/src/app/page.js
@@ -1,4 +1,6 @@
-import Head from "next/head";
+"use client";
+
+import Carousel from "./components/CarouselFeature";
 
 export default function Home() {
   return (
@@ -14,14 +16,30 @@ export default function Home() {
           </div>
           <img src="/asset_1.webp" className="w-90"></img>
       </section>
-      {/* ABOUT US SECTION */}
+      {/* KEY FEATURES SECTION */}
       <section className="container flex flex-col justify-center">
         <header className="flex flex-row items-center justify-between w-full">
-            <h1 className="font-bold text-6xl ml-20">What is <i className="tracking-tighter">Corefocus</i>?</h1>
+            <h1 className="font-bold text-6xl ml-20">Explore our key <i className="tracking-tighter">features</i></h1>
             <p className="font-light text-xl mr-20">Designed to help you stay on track, effortlessly</p>
         </header>
         {/* To hold all the cards */}
-        <div className="flex flex-wrap p-3 gap-10 ml-20"> 
+         <div className="flex flex-wrap justify-between items-center  p-3 m-20 mt-10 bg-gray-300/20 rounded-2xl"> 
+          <div className="m-10 bg-gray-300/40">
+            <h2>We have a range of key features</h2>
+          </div>
+          <div style={{ height: '400px', position: 'relative' }} className="m-30">
+            <Carousel
+              baseWidth={400}
+              autoplay={true}
+              autoplayDelay={3000}
+              pauseOnHover={true}
+              loop={true}
+              round={false}
+            />
+          </div>
+        </div>
+
+        {/* <div className="flex flex-wrap p-3 gap-10 ml-20"> 
           <div className="bg-gray-300/40 w-70 min-h-60 mt-10 rounded-lg flex flex-col justify-center">
             <span className="flex items-center p-3 justify-center gap-3">
               <i className="fa-solid fa-bell text-lg text-blue-400 text-xl"></i>
@@ -45,8 +63,9 @@ export default function Home() {
             </span>
             <p className="p-3 font-semibold text-xl">Reminders everyday to keep you on track with your desired goals.</p>
           </div>
-        </div>
+        </div> */}
       </section>
+
     </main>
   );
 }
