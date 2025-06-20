e3de7f83a6909cb2d7620784c2ff5be98874b7e9
srahman14 <s.rahman14@hotmail.com>
2025-06-21 00:01:10 +0100

    firebase setup

---
+++ corefocus-w/src/app/firebase-config.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyA79tot_cv9RQvQqvfg20Lw4NUk7Fxxj0Y",
+  authDomain: "corefocus-7fd97.firebaseapp.com",
+  projectId: "corefocus-7fd97",
+  storageBucket: "corefocus-7fd97.firebasestorage.app",
+  messagingSenderId: "865725210747",
+  appId: "1:865725210747:web:7fe394f46911b6ec5bc2de"
+};
+
