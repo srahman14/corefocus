b134965a93d94adb2fafc355a90181ba53ba149b
srahman14 <s.rahman14@hotmail.com>
2025-06-21 00:00:51 +0100

    responsiveness added

--- corefocus-w/src/app/(main)/page.js
+++ corefocus-w/src/app/(main)/page.js
@@ -5,6 +5,7 @@ import SpotlightCard from "../components/SpotlightCard";
 import AnimatedContent from "../components/AnimatedContent";
 import ImageMarquee from "../components/ReactMarquee";
 import FAQItem from "../components/FAQComponent";
+import Link from "next/link";
 
 export default function Home() {
 
@@ -30,21 +31,21 @@ export default function Home() {
                 backgroundSize: '45px 45px',
               }}>
               <div>
-                <h1 className="font-bold text-left lg:text-7xl md:text-7xl sm:text-6xl mb-6 tracking-tighter max-w-200">
+                <h1 className="font-bold text-left text-5xl md:text-6xl lg:text-7xl mb-6 tracking-tighter max-w-200">
                   Master your <i className="underline decoration-sky-500">habits</i> <br /> Achieve what <i className="underline decoration-sky-500">matters</i>
                 </h1>              
-                <p className="font-light lg:text-3xl md:text-7xl sm:text-6xl mb-12 tracking-tight text-gray-500 w-120 text-left">
+                <p className="font-light text-3xl md:text-4xl lg:text-5xl mb-12 tracking-tight text-gray-500 w-120 text-left">
                   Build consistent routines, align your actions with your goals, and stay accountable.
                 </p>
               </div>
 
               <div className="flex mt-30 x-space-4 justify-start">
-                  <a href="#" className="bg-[#222] text-white mr-4 hover:bg-[#222]/90 p-5 rounded-3xl font-semibold tracking-tighter text-2xl">Get Started</a>
-                  <a href="#" className="bg-[#222] text-white hover:bg-[#222]/90 p-5 rounded-3xl font-semibold tracking-tighter text-2xl">Explore Plans</a>
+                  <Link href="/signup" className="bg-[#222] text-white mr-4 hover:bg-[#222]/90 p-5 rounded-4xl font-semibold tracking-tighter text-xl md:text-2xl">Get Started</Link>
+                  <Link href="/" className="bg-[#222] text-white hover:bg-[#222]/90 p-5 rounded-4xl font-semibold tracking-tighter text-xl md:text-2xl">Explore Plans</Link>
               </div>
 
             </div>
-            <img src="/asset_6.png" className="w-120 h-120 rounded-3xl"></img>
+            <img src="/asset_6.png" className="hidden lg:block w-[30rem] h-[30rem] rounded-3xl"></img>
           </div>
         </AnimatedContent>
         
@@ -63,9 +64,9 @@ export default function Home() {
          <div className="flex flex-wrap justify-center gap-8 items-center p-3 m-20 mt-10 rounded-2xl"> 
           <div className="p-2 flex flex-col items-center rounded-2xl ">              
               {/* NEED TO ADD LINKS LATER ON */}
-              <div className="flex flex-row flex-wrap gap-12">
+              <div className="flex flex-row flex-wrap gap-12 items-center justify-center">
                   <Magnet padding={50} disabled={false} magnetStrength={13}>
-                      <div className="container bg-[#222] text-white p-2 rounded-lg min-h-100 max-w-100 cursor-default">
+                      <div className="container bg-[#222] text-white p-2 rounded-lg min-h-100 max-w-90 cursor-default">
                           <span className="flex justify-between items-center text-3xl m-3">
                             <p className="font-bold tracking-tighter text-center">Habit Tracker</p>
                             <i className="fa-solid fa-bell"></i>
@@ -82,7 +83,7 @@ export default function Home() {
                   </Magnet>
 
                   <Magnet padding={50} disabled={false} magnetStrength={13}>
-                      <div className="container bg-[#222] text-white p-2 rounded-lg min-h-100 max-w-100 cursor-default">
+                      <div className="container bg-[#222] text-white p-2 rounded-lg min-h-100 max-w-90 cursor-default">
                           <span className="flex justify-between m-3 items-center text-3xl">
                             <i className="fa-solid fa-chart-area"></i>
                             <p className="font-bold p-3 cursor-pointer tracking-tighter text-center">Goal Board</p>
@@ -98,7 +99,7 @@ export default function Home() {
                   </Magnet>
 
                   <Magnet padding={50} disabled={false} magnetStrength={13}>
-                      <div className="container bg-[#222] text-white p-2 rounded-lg min-h-100 max-w-100 cursor-default">
+                      <div className="container bg-[#222] text-white p-2 rounded-lg min-h-100 max-w-90 cursor-default">
                           <span className="flex justify-between m-3 items-center text-3xl">
                             <i className="fa-solid fa-list-check"></i>
                             <p className="font-bold p-3 cursor-pointer tracking-tighter text-center">Task Sync</p>
@@ -114,7 +115,7 @@ export default function Home() {
                   </Magnet>
 
                   <Magnet padding={50} disabled={false} magnetStrength={13}>
-                      <div className="container bg-[#222] text-white p-2 rounded-lg min-h-100 max-w-100 cursor-default">
+                      <div className="container bg-[#222] text-white p-2 rounded-lg min-h-100 max-w-90 cursor-default">
                           <span className="flex justify-between m-3 items-center text-3xl">
                             <i className="fa-solid fa-pen-to-square"></i>
                             <p className="font-bold p-3 cursor-pointer tracking-tighter text-center">Read Route</p>
@@ -130,7 +131,7 @@ export default function Home() {
                   </Magnet>
 
                   <Magnet padding={50} disabled={false} magnetStrength={13}>
-                      <div className="container bg-[#222] text-white p-2 rounded-lg min-h-100 max-w-100 cursor-default">
+                      <div className="container bg-[#222] text-white p-2 rounded-lg min-h-100 max-w-90 cursor-default">
                           <span className="flex justify-between m-3 items-center text-3xl">
                             <i className="fa-solid fa-stopwatch"></i>
                             <p className="font-bold p-3 cursor-pointer tracking-tighter text-center">Focus Camp</p>
@@ -159,22 +160,24 @@ export default function Home() {
           animateOpacity
           threshold={0.2}
           >
-            <header className="flex flex-row items-center justify-center w-full">
+            <header className="flex items-center justify-center mr-8">
                 <h1 className="font-bold text-5xl ml-20">How to get <i className="tracking-tighter">started?</i></h1>
             </header>
 
-            <div className="flex flex-wrap flex-row justify-center items-center p-3 m-20 mt-10"> 
-              <SpotlightCard className="custom-spotlight-card bg-[#222] m-10 min-h-100 w-92 text-center cursor-default text-white flex flex-col items-center justify-center" spotlightColor="rgba(234, 230, 238, 0.2)">
+            <div className="flex flex-row flex-wrap justify-center items-center p-3 m-20 mt-10"> 
+              <SpotlightCard 
+              className="custom-spotlight-card bg-[#222] m-10 sm:min-h-85 sm:w-82 lg:min-h-100 lg:w-92 text-center cursor-default text-white flex flex-col items-center justify-center" 
+              spotlightColor="rgba(234, 230, 238, 0.2)">
                 <h2 className="text-4xl font-bold mb-3">Step 1: Sign-Up</h2>
                 <p>Create an account to join the Corefocus community today</p>
               </SpotlightCard>
 
-              <SpotlightCard className="custom-spotlight-card m-10 min-h-100 w-92 text-center cursor-default text-white flex flex-col items-center justify-center" spotlightColor="rgba(234, 230, 238, 0.2)">
+              <SpotlightCard className="custom-spotlight-card m-10 sm:min-h-85 sm:w-82 md:w-75 lg:min-h-100 lg:w-92 text-center cursor-default text-white flex flex-col items-center justify-center" spotlightColor="rgba(234, 230, 238, 0.2)">
                 <h2 className="text-4xl font-bold mb-3">Step 2: Set Habits & End Goals</h2>
                 <p>Establish long-term and short-term commitments to meet your goals</p>
               </SpotlightCard>
 
-              <SpotlightCard className="custom-spotlight-card m-10 min-h-100 w-92 text-center cursor-default text-white flex flex-col items-center justify-center" spotlightColor="rgba(234, 230, 238, 0.2)">
+              <SpotlightCard className="custom-spotlight-card m-10 sm:min-h-85 sm:w-82 md:w-75 lg:min-h-100 lg:w-92 text-center cursor-default text-white flex flex-col items-center justify-center" spotlightColor="rgba(234, 230, 238, 0.2)">
                 <h2 className="text-4xl font-bold mb-3">Step 3: Track & Reflect</h2>
                 <p>Regularly check-in to maintain your goals and reflect on wins and shortcomings</p>
               </SpotlightCard>
@@ -184,13 +187,13 @@ export default function Home() {
 
     {/* FAQ SECTION  */}
     <section className="container flex flex-col justify-center">
-      <div className="flex flex-row items-start justify-between m-16">
-        <div className="w-75">
+      <div className="flex flex-col items-center justify-center lg:flex-row lg:items-start lg:justify-between lg:m-16 gap-8">
+        <div className="w-75 mr-20">
           <h2 className="text-6xl font-semibold">FAQs</h2>
           <p className="text-2xl font-light">Got a different question? Email support@corefocus.ai</p>
         </div>
 
-        <div className="mr-20 w-[70%] max-h-[400px] overflow-y-auto pr-4">
+        <div className="w-[70%] max-h-[400px] overflow-y-auto pr-4">
           <FAQItem
             question="How do I create an account?"
             answer="The steps to create an account can be found here."
