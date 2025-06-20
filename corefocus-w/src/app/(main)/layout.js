6a7bab4c6aa6fbdc0d7d78892d007adfbcce9c04
srahman14 <s.rahman14@hotmail.com>
2025-06-21 00:00:34 +0100

    html tags removed to avoid mounted conflicts

--- corefocus-w/src/app/(main)/layout.js
+++ corefocus-w/src/app/(main)/layout.js
@@ -16,21 +16,10 @@ export const metadata = {
 
 export default function MainLayout({ children }) {
   return (
-  <html lang="en" className={`${montserrat.variable}`}>
-      <head>
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
-          integrity="sha512-...." // Optional: add integrity for security
-          crossOrigin="anonymous"
-          referrerPolicy="no-referrer"
-        />
-      </head>
-      <body>
+      <div>
         <Navbar /> 
         {children}
         <Footer />
-      </body>
-    </html>
+      </div>
   );
 }
