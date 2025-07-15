d6e5c8b50501eaf8f8a58df756f7406ac4ac01e5
srahman14 <s.rahman14@hotmail.com>
2025-07-15 20:40:58 +0100

    Fixed logout functionality and signUpWithEmail function

--- corefocus-w/src/app/context/AuthContext.jsx
+++ corefocus-w/src/app/context/AuthContext.jsx
@@ -1,68 +1,27 @@
 "use client";
 
 import React, { createContext, useContext, useEffect, useState } from "react";
-import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
+import { getAuth, onAuthStateChanged, signOut, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
 import { auth, db } from "../firebase";
 import { doc, getDoc, setDoc } from "firebase/firestore";
 import { useRouter } from "next/navigation";
 
 const AuthContext = createContext();
 
-const signUpWithEmail = async (email, password) => {
-  try {
-    const userCredential = await createUserWithEmailAndPassword(
-      auth,
-      email,
-      password
-    );
-    
-    await setDoc(doc(db, "users", user.uid), {
-        email: user.email,
-        completedSteps: false,
-        createdAt: new Date(),
-    });
-
-    return user
-  } catch (error) {
-    console.log("errorCode:", error.code, "errorMessage:", error.message);
-    throw error;
-  }
-};
-
-const signInWithEmail = async (email, password) => {
-    try {
-      const userCredential = await signInWithEmailAndPassword(
-        auth,
-        email,
-        password
-      );
-      return userCredential.user;
-    } catch (error) {
-      console.log("Sign in error:", error.code, error.message);
-      throw error;
-    }
-};
- 
-const { logout } = async () => {
-    const router = useRouter();
-
-    try {
-        await signOut(auth);
-        console.log("Logging out")
-        router.push("/");
-    } catch (error) {
-        console.log("Error signing out: ", error);
-    }
-}
-
 export const AuthProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(null);
   const [userData, setUserData] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [authError, setAuthError] = useState(null);
+
+  const router = useRouter();
+  const auth = getAuth();
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, async (user) => {
       setCurrentUser(user);
+      setLoading(false);
+      
       if (user) {
         const docRef = doc(db, "users", user.uid);
         const docSnap = await getDoc(docRef);
@@ -75,14 +34,84 @@ export const AuthProvider = ({ children }) => {
       } else {
         setUserData(null);
       }
-      setLoading(false);
     });
 
     return () => unsubscribe();
-  }, []);
+  }, [auth]);
+
+    const logout = async () => {
+    const auth = getAuth();
+
+    try {
+      await signOut(auth);
+      console.log("Logging out successfully");
+      router.push("/")
+    } catch(error) {
+      console.log("Error signing out: ", error)
+    }
+  };
+
+  const signUpWithEmail = async (email, password) => {
+    setLoading(true);
+    setAuthError(null);
+
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      const user = userCredential.user;
+
+      await setDoc(doc(db, "users", user.uid), {
+          email: user.email,
+          completedSteps: false,
+          createdAt: new Date(),
+      });
+
+      return userCredential;
+
+    } catch (error) {
+      console.log("errorCode:", error.code, "errorMessage:", error.message);
+      setAuthError(error.message);
+      throw error;
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const signInWithEmail = async (email, password) => {
+    setLoading(true);
+    setAuthError(null);
+
+      try {
+        const userCredential = await signInWithEmailAndPassword(
+          auth,
+          email,
+          password
+        );
+        return userCredential.user;
+      } catch (error) {
+        console.log("Sign in error:", error.code, error.message);
+        setAuthError("Sign in error: Credential(s) entered invalid")
+        throw error;
+      } finally {
+        setLoading(false);
+      }
+  };
+
+  const value = {
+    currentUser, 
+    userData,
+    logout,
+    authError,
+    loading,
+    signInWithEmail,
+    signUpWithEmail,
+  }
 
   return (
-    <AuthContext.Provider value={{ currentUser, userData, loading, signUpWithEmail, signInWithEmail, logout}}>
+    <AuthContext.Provider value={value}>
       {!loading && children}
     </AuthContext.Provider>
   );
