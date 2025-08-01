2a0c693b79ecb2d8f116455a2a7f6b355c994733
srahman14 <s.rahman14@hotmail.com>
2025-08-01 20:08:53 +0100

    Sidebar action component

---
+++ corefocus-w/src/app/components/SidebarComponents/SidebarActionItem.jsx
@@ -0,0 +1,11 @@
+export function SidebarActionItem({ icon, label, collapsed, onClick }) {
+  return (
+    <div
+      onClick={onClick}
+      className="w-full flex items-center text-violet-400 px-4 py-3 hover:bg-gray-800 transition cursor-pointer group"
+    >
+      <i className={`fa-solid ${icon} text-lg`} />
+      {!collapsed && <span className="ml-3 font-bold">{label}</span>}
+    </div>
+  );
+}
