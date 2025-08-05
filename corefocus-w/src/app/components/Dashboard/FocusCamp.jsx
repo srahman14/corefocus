e379644a2c8d33d3513f27fa01b8ecf49282c2ad
srahman14 <s.rahman14@hotmail.com>
2025-08-05 20:47:44 +0100

    added PomodoroTimer component

--- corefocus-w/src/app/components/Dashboard/FocusCamp.jsx
+++ corefocus-w/src/app/components/Dashboard/FocusCamp.jsx
@@ -1,8 +1,9 @@
+import PomdoroTimer from "../Pomdoro/PomdoroTimer";
 export default function FocusCamp() {
   return (
     <div className="bg-gray-900/90 p-6 rounded-xl shadow text-white min-h-[200px]">
       <h2 className="text-xl font-semibold mb-2">🎯 Focus Camp</h2>
-      <p className="text-sm text-gray-300">Pomodoro timer.</p>
+      <PomdoroTimer />
     </div>
   );
 }
