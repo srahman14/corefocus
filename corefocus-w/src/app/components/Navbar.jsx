eb9b24fe1ef401b85be24ec9ac9bf6b5563d17f2
srahman14 <s.rahman14@hotmail.com>
2025-08-08 17:12:34 +0100

    updated UI for navbar made it smaller

--- corefocus-w/src/app/components/Navbar.jsx
+++ corefocus-w/src/app/components/Navbar.jsx
@@ -2,10 +2,13 @@
 
 import { useState, useEffect } from "react";
 import Link from "next/link";
+import { useThemeStore } from "../store/useThemeStore";
+import Magnet from "./Magnet";
 
 export default function Navbar() {
   const [isScrolled, setIsScrolled] = useState(false);
   const [lastScrollY, setLastScrollY] = useState(0);
+  const { isDark, toggleTheme } = useThemeStore();
 
   const handleScroll = () => {
     const currentScrollY = window.scrollY;
@@ -27,30 +30,37 @@ export default function Navbar() {
 
   return (
     <nav
-      className={`transition-all duration-400 fixed top-0 left-1/2 transform -translate-x-1/2 ${
+      className={`transition-all duration-300 ease-in-out fixed top-0 left-1/2 transform -translate-x-1/2 ${
         isScrolled
-          ? "w-11/12 mt-4 bg-gray-500/50 backdrop-blur shadow-lg rounded-lg border-b-2 border-b-transparent"
-          : "w-full mt-2 bg-transparent border-b-2 border-b-gray-200"
-      } py-4 px-8 z-50`}
-    >
+          ? "w-10/14 mt-8 bg-gray-500/50 backdrop-blur shadow-lg rounded-3xl border border-transparent"
+          : "w-10/14 mt-8 bg-gray-500/50 backdrop-blur border border-gray-500 rounded-3xl"
+      } py-4 px-8 z-50`
+        }
+    > 
       <div className="flex justify-between items-center">
         <div className="text-xl font-bold">
-          <span className="flex items-center gap-1">
-            <img src="/logo.svg" className="w-8 rounded-lg"></img>
-            <p className="tracking-tighter font-semibold">CoreFocus</p>
+          <span className={`flex items-center gap-1 ${isDark ? "text-white" : "text-black"}`}>
+            {/* <img src="/logo.svg" className="w-14 rounded-lg"></img> */}
+            <p className="tracking-tighter font-semibold text-2xl"><i>c</i>orefocus</p>
           </span>
         </div>
 
-        <ul className="flex space-x-8">
-          <li className="hover:text-blue-600 transition-all cursor-pointer font-semibold tracking-tighter text-lg p-1">
+        <ul className={`flex space-x-8 ${isDark ? "text-white" : "text-black"}`}>
+          <li className="hover:text-violet-600 cursor-pointer font-semibold tracking-tighter text-xl p-1">
             <Link href="/">Home</Link>
           </li>
-          <li className="hover:text-blue-600 cursor-pointer font-semibold tracking-tighter text-lg p-1">
+          <li className="hover:text-violet-600 cursor-pointer font-semibold tracking-tighter text-xl p-1">
             <Link href="/">Plans</Link>
           </li>
-          <li className="hover:text-blue-600 cursor-pointer font-semibold tracking-tighter text-lg p-1">
+          <li className="hover:text-violet-600 cursor-pointer font-semibold tracking-tighter text-xl p-1">
             <Link href="/login">Login</Link>
           </li>
+          <button
+          onClick={toggleTheme}
+          className="px-3 cursor-pointer rounded-lg bg-violet-500 text-white hover:bg-violet-600"
+          >            
+            {isDark ? <i className="fa-jelly fa-regular fa-sun"></i> : <i className="fa-jelly fa-regular fa-moon"></i>}
+          </button>
         </ul>
       </div>
     </nav>
