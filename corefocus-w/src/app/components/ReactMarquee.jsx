4ddb18c49e6e9cab45816fb4ba7b46fa58cbb0d3
srahman14 <s.rahman14@hotmail.com>
2025-08-08 17:12:48 +0100

    updated stylings for marquee

--- corefocus-w/src/app/components/ReactMarquee.jsx
+++ corefocus-w/src/app/components/ReactMarquee.jsx
@@ -4,41 +4,41 @@ const cards = [
   {
     title: "Habit Tracker",
     description: "Track your daily habits like Salah, Qur’an, workouts, and sleep — all in one clean dashboard.",
-    icon: <i className="fa-solid fa-bell h-[16px] w-[16px] text-black" />,
+    icon: <i className="fa-solid fa-bell h-[16px] w-[16px] text-[#520dd0]" />,
   },
   {
     title: "GoalBoard",
     description: "Set and reflect on your monthly and yearly goals. Build discipline with intentional planning.",
-    icon: <i className="fa-solid fa-chart-area h-[16px] w-[16px] text-black" />,
+    icon: <i className="fa-solid fa-chart-area h-[16px] w-[16px] text-[#520dd0]" />,
   },
   {
     title: "TaskSync",
     description: "Integrate with your calendar to sync daily to-dos and spiritual goals effortlessly.",
-    icon: <i className="fa-solid fa-list-check h-[16px] w-[16px] text-black" />,
+    icon: <i className="fa-solid fa-list-check h-[16px] w-[16px] text-[#520dd0]" />,
   },
   {
     title: "ReadRoute",
     description: "Capture insights from your readings and reflections with our minimal journaling system.",
-    icon: <i className="fa-solid fa-pen-to-square h-[16px] w-[16px] text-black" />,
+    icon: <i className="fa-solid fa-pen-to-square h-[16px] w-[16px] text-[#520dd0]" />,
   },
   {
     title: "FocusCamp",
     description: "Use a goal-based timer to stay focused during Qur’an study, workouts, or deep work.",
-    icon: <i className="fa-solid fa-stopwatch h-[16px] w-[16px] text-black" />,
+    icon: <i className="fa-solid fa-stopwatch h-[16px] w-[16px] text-[#520dd0]" />,
   },
 ];
 
 export default function ImageMarquee() {
   return (
-    <Marquee gradient={true} speed={35}>
+    <Marquee gradient={true} gradientColor="black" speed={35} className="rounded-xl">
       {[...cards, ...cards].map((card, idx) => (
         <div
           key={idx}
-          className="flex flex-col cursor-pointer items-center justify-center text-center bg-white rounded-lg p-4 mx-4 w-52 h-55"
+          className="flex flex-col cursor-pointer items-center justify-center text-center bg-black-500 rounded-lg p-4 mx-4 w-52 h-55"
         >
-          <div className="text-green-600 text-4xl mb-2 mr-4">{card.icon}</div>
+          <div className="text-4xl mb-2 mr-4">{card.icon}</div>
           {/* THESE H3s WILL BECOME LINKS IN THE FUTURE, THE LINK WILL BE ADDED TO CARDS LIST */}
-          <h3 className="text-lg font-semibold mb-1">{card.title}</h3>
+          <h3 className="text-lg font-semibold mb-1 text-white">{card.title}</h3>
         </div>
       ))}
     </Marquee>
