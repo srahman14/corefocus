46761598f874ce7f4397e687e2f3ce5f020c6cb6
srahman14 <s.rahman14@hotmail.com>
2025-08-10 22:40:04 +0100

    updated UI for sidebar

--- corefocus-w/src/app/components/SidebarComponents/SidebarItem.jsx
+++ corefocus-w/src/app/components/SidebarComponents/SidebarItem.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export function SidebarItem({ href, icon, label, collapsed }) {
   return (
     <Link href={href}>
-      <div className="w-full flex items-center text-violet-400 px-4 py-3 hover:bg-gray-800 transition cursor-pointer group">
+      <div className="w-full flex items-center text-[#520dd0] px-4 py-3 hover:text-[#520dd0]/70 transition cursor-pointer group">
         <i className={`fa-solid ${icon} text-lg`} />
         {!collapsed && <span className="ml-3 font-bold">{label}</span>}
       </div>
