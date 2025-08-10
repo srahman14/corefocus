152ba0137258fc7c907aac52bd557d99d6440343
srahman14 <s.rahman14@hotmail.com>
2025-08-10 22:38:49 +0100

    theme store

--- corefocus-w/src/app/store/useThemeStore.js
+++ corefocus-w/src/app/store/useThemeStore.js
@@ -1,11 +1,19 @@
 // app/store/useThemeStore.js
-import { create } from 'zustand';
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-export const useThemeStore = create((set) => ({
-  isDark: false,
-  toggleTheme: () =>
-    set((state) => {
-      console.log('Toggling theme...'); // <-- this should show in console
-      return { isDark: !state.isDark };
+export const useThemeStore = create(
+  persist(
+    (set) => ({
+      isDark: false,
+      toggleTheme: () =>
+        set((state) => {
+          console.log("Toggling theme...");
+          return { isDark: !state.isDark };
+        }),
     }),
-}));
+    {
+      name: "theme-storage", // key in localStorage
+    }
+  )
+);
