e1e9b598b44fc1d6d64523648b1a59e83262b791
srahman14 <s.rahman14@hotmail.com>
2025-08-12 19:13:09 +0100

    updated stylings - added light and dark theme

--- corefocus-w/src/app/components/SidebarComponents/SidebarActionItem.jsx
+++ corefocus-w/src/app/components/SidebarComponents/SidebarActionItem.jsx
@@ -2,7 +2,7 @@ export function SidebarActionItem({ icon, label, collapsed, onClick }) {
   return (
     <div
       onClick={onClick}
-      className="w-full flex items-center text-[#520dd0] px-4 py-3 hover:text-[#520dd0]/70 transition cursor-pointer group"
+      className="w-full flex items-center text-[#7E4E9E] dark:text-[#520dd0] px-4 py-3 hover:text-[#520dd0]/70 transition cursor-pointer group"
     >
       <i className={`fa-solid ${icon} text-lg`} />
       {!collapsed && <span className="ml-3 font-bold">{label}</span>}
